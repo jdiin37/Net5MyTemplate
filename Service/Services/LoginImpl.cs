cad46c8f07c0fd82ce04e1d96ec427b5a90643ff
郭璁翰 <mattkuo@linkchain.tw>
2021-08-05 10:23:50 +0800

    AddNunit

---
+++ Service/Services/LoginImpl.cs
@@ -0,0 +1,21 @@
+﻿using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    public class LoginImpl : ILogin
+    {
+        public bool UserLogin(string userAccount, string userPassword)
+        {
+            if(userAccount == "1" && userPassword == "1")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ServiceTest/Service/LoginTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Service.Interfaces;
+using Service.Services;
+
+namespace ServiceTest.Service
+{
+    class LoginTest
+    {
+
+        ILogin loginService = new LoginImpl();
+
+        [Test]
+        public void TestUserLogin()
+        {
+            if (loginService.UserLogin("1","1"))
+            {
+
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
