ebc29b6e4e02fc894ac00a331b9eb7e289758444
郭璁翰 <mattkuo@linkchain.tw>
2021-10-15 16:50:21 +0800

    addRepository

---
+++ Repository/Interface/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interface
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        IQueryable<T> FindAll();
+        T FindById(int id);
+        int Insert(T entity);
+        int Update(T entity);
+        void Delete(int id);
+        int Upsert(T entity);
+    }
+}
---
+++ Service/Entity/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Entity
+{
+    class User
+    {
+        public string name { get; set; }
+
+        public string gender { get; set; }
+
+        public string mobile { get; set; }
+    }
+}
